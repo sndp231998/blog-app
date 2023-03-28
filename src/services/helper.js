8c234f543490ef3d712edabca6cdc7a703dbc276
sndp231998 <sandipchapagain797@gmail.com>
2023-03-28 23:05:49 +0545

    first

---
+++ src/services/helper.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { getToken } from "../auth";
+
+export const BASE_URL= "https://backend-blogging-application-production.up.railway.app/api/v1";
+//export const BASE_URL = "http://localhost:9090/api/v1";
+// export const BASE_URL = "https://apis.lcwdblogs.online/api/v1";
+
+export const myAxios = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const privateAxios = axios.create({
+  baseURL: BASE_URL,
+});
+
+privateAxios.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers.common.Authorization = `Bearer ${token}`;
+      // console.log(config);
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
