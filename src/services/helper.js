9f15d153ed8dfc295017bad193674b75304fadbc
sandip chapagain <sandipchapagain797@gmail.com>
2023-08-05 17:55:09 +0545

    Update helper.js
    sec

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { getToken } from "../auth";
 
-//export const BASE_URL= "https://backend-blogging-application-production.up.railway.app/api/v1";
-export const BASE_URL = "http://localhost:9090/api/v1";
+
+export const BASE_URL= "https://blog-app-apis-masterr-production.up.railway.app/api/v1";
+//export const BASE_URL = "http://localhost:9090/api/v1";
 
 
 export const myAxios = axios.create({
